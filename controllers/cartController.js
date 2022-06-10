121daec0135d0d2c17e4ace434a7fd3a944594a6
Eban Godwin ikoni <ikonicityairban@gmail.com>
2022-06-10 13:07:29 +0100

    still cleaning :D

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,11 +1,10 @@
 import Cart from "../models/Cart";
 import Product from "../models/Product";
-
 import { StatusCodes } from "http-status-codes";
 import CustomError from "../errors";
 import { checkPermissions } from "../utils";
 
-//-getAllCarts
+//HEAD:           GET ALL CART --specific for the admin--         //
 export const getAllCarts = async (req, res) => {
   const cart = await Cart.find({})
     .sort("-total")
@@ -13,7 +12,7 @@ export const getAllCarts = async (req, res) => {
   res.status(StatusCodes.OK).json({ cart, count: cart.length });
 };
 
-//-getSingleCarts
+//HEAD:       GET LOGGED IN USER'S CART   --only for logged in user and admin--
 export const getCurrentUserCart = async (req, res) => {
   const { id: cartId } = req.params;
   const cart = await Cart.findOne({ _id: cartId });
@@ -30,20 +29,23 @@ export const getCurrentUserCart = async (req, res) => {
   }
 };
 
-//- Add Itens to cart
+//HEAD          ADD PRODUCTS TO THE CART
 export const addToCart = async (req, res) => {
-  const cart = await Cart.findOneAndUpdate(
-    { userID: req.params.id },
-    req.body,
-    {
-      runValidators: true,
-      new: true,
-    }
-  );
+  const { product } = req.body;
+  if (!product) {
+    throw new CustomError.BadRequestError("no product provided");
+  }
+  const cart = await Cart.findOneAndUpdate({ userID: req.params.id }, product, {
+    runValidators: true,
+    new: true,
+  });
   res.status(StatusCodes.OK).json({ cart });
 };
 
-export const clearCart = async (_req, res) => {
-  await Cart.c().where("products").gte(1);
+export const clearCart = async (req, res) => {
+  await Cart.findOne({ user: req.userId });
+  checkPermissions(req.user, cart.userId);
+  cart.products = [];
+  await cart.save();
   res.status(StatusCodes.OK).json({ msg: "cart cleared ❗" });
 };
